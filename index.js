15be4bf2ff8a85e41f806bdcf57d1b0c6b800610
david <davidwnewman91@gmail.com>
2019-05-09 16:54:00 -0500

    just delete gosh darn it!

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 console.log('index.js connected')
+let expenses = {}
 let costs = []
 
 function addBill(){
@@ -12,34 +13,37 @@ function addBill(){
     li2.innerText = ('$' + document.getElementById('amount').value);
     document.getElementById('items').appendChild(li2);
     console.log(li2)
+    
+    //Log for deletion(obj)
+    expenses[document.getElementById('name').value] = document.getElementById('amount').value;
     //create trash cans
     let trash_can = document.createElement('input');
     trash_can.setAttribute('type', 'image')
     trash_can.setAttribute('src', 'assets/trash_can.svg')
+    trash_can.setAttribute('width', '20vw')
+    trash_can.setAttribute('height', '20vh')
     document.querySelector('.trash').appendChild(trash_can);
     //add total to the top
     function budgetTotal(){
         // make array with num vs str
         costs.push(
             parseFloat(document.getElementById('amount').value));
-        console.log(costs)
-        // let Total = total.reduce();
-        let reducer = function(accumulator, currentValue){
-            return accumulator + currentValue;
+            console.log(costs)
+            // let Total = total.reduce();
+            let reducer = function(accumulator, currentValue){
+                return accumulator + currentValue;
+            }
+            let Total = costs.reduce(reducer);
+            // console.log(Total)
+            //update top with current budget totals
+            let h2 = document.createElement('h2');
+            h2.innerText = `Budget Total: $${Total}`;
+            document.querySelector('h2').remove();
+            document.querySelector('.header-text').append(h2);
         }
-        let Total = costs.reduce(reducer);
-        console.log(Total)
-        //update top with current budget totals
-        let h2 = document.createElement('h2');
-        h2.innerText = `Budget Total: $${Total}`;
-        document.querySelector('h2').remove();
-        document.querySelector('.header-text').append(h2);
+        console.log(expenses)
+        budgetTotal()
     }
-    budgetTotal()
-}
-
+    
 //Trash can functions
-// document.querySelectorAll('.trash').addEventListener('click', function(){
-//     //delete li[] & li2[] = 2li[]
-//     document.querySelector('#item')[document.querySelectorAll('.trash')].remove();
-// });
+// trash[i] deletes obj[i]
